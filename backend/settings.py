cf519262f21d95e7073c831948736fd1d0620610
Mohamed Sami <119891184+MohamedSami852@users.noreply.github.com>
2025-01-01 04:15:54 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,13 +25,12 @@ SECRET_KEY = "django-insecure-fh9-$@*4t7c$5*x9on1-%z!#svehd&m_-egtpkby+1e1!1eul#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',
-                 'reptrack-production.up.railway.app',
+ALLOWED_HOSTS = ['https://reptrack.azurewebsites.net/'
                  ]
 # settings.py
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 CSRF_TRUSTED_ORIGINS = [
-    'https://reptrack-production.up.railway.app',
+    'https://reptrack.azurewebsites.net/'
 ]
 
 
